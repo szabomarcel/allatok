0149d1858c5f415afbd2b6c79da072c6f1275c4c
szabomarcel <szabomarcell34@gmail.com>
2023-10-17 21:38:28 +0200

    allatok

--- login/allatkivalasztas.php
+++ login/allatkivalasztas.php
@@ -23,11 +23,20 @@
     }
     
 ?>
-<form method="post" action="index.php?menu=home&id=<?php echo $adatok['allatid'];?>" enctype="multipart/form-data">
-    <input type="hidden" name="allatid" value="<?php echo $adatok['allatid'];?>">
+<!--array (size=8)
+  'allatid' => string '7' (length=1)
+  'allat_neve' => string 'Gazsi' (length=5)
+  'faj' => string 'kutya' (length=5)
+  'fajta' => string 'Mobsz' (length=5)
+  'szuletesi_ido' => string '2021-01-11' (length=10)
+  'nem' => string 'szuka' (length=5)
+  'megjegyzes' => string 'csinos' (length=6)
+  'nyilvantartasban' => string '2023-05-10' (length=10) -->
+<form method="post"  enctype="multipart/form-data">
+    <input type="hidden" name="allatid" >
     <div class="mb-3">
-        <label for="allat_neve" class="form-label">Állat neve: </label>
-        <input type="text" class="form-control" name="allat_neve" id="allat_neve" aria-describedby="allat_neveHelp" value="<?php echo $adatok['allat_neve'];?>">
+        <label for="allat_neve" class="form-label">Állat neve</label>
+        <input type="text" class="form-control" name="allat_neve" id="allat_neve">
     </div>
     <div class="row">
         <div class="mb-3 col-6">
@@ -45,24 +54,24 @@
             </select>            
         </div>
         <div class="mb-3 col-6">
-            <label for="fajtaselect" class="form-label">Fajta</label>            
-            <select name="fajtaselect" id="fajtaselect" class="form-select">
+            <label for="fajtaSelect" class="form-label">Állatfajta</label>
+            <select id="fajtaSelect" name="fajtaSelect" class="form-select">
                 <?php
-                foreach ($db ->getFajok() as $value){
-                    if($adatok['faj'] == $value){
-                        echo '<option select value="'.$value[0].'">'.$value[0].'</option>';
-                    }else{
-                        echo '<option value="'.$value[0].'">'.$value[0].'</option>';
+                foreach ($db->getFajtak() as $value) {
+                    if ($adatok['fajta'] == $value[0]) {
+                        echo '<option selected value="' . $value[0] . '">' . $value[0] . '</option>';
+                    } else {
+                        echo '<option value="' . $value[0] . '">' . $value[0] . '</option>';
                     }
                 }
                 ?>
-            </select>            
+            </select>
         </div>
     </div>
     <div class="row">
         <div class="mb-3 col-6">
             <label for="szuletesi_ido" class="form-label">Dátum: </label>
-            <input type="date" class="form-control" name="szuletesi_ido" id="szuletesi_ido" aria-describedby="szuletesi_idoHelp" max="<?php echo date("Y-m-d");?>" value="<?php echo $adatok['szuletesi_ido'];?>">
+            <input type="date" class="form-control" name="szuletesi_ido" id="szuletesi_ido" aria-describedby="szuletesi_idoHelp" max="<?php echo date("Y-m-d");?>" >
         </div>    
         <div class="mb-3 col-6">
             <label for="nem" class="form-label">Az állat neme: </label>
@@ -75,11 +84,11 @@
     <div class="row">
          <div class="mb-3 col-6">
             <label for="nyilvantartas" class="form-label">Nyilvántartásban vétel: </label>
-            <input type="date" class="form-control" name="nyilvantartas" id="nyilvantartas" aria-describedby="nyilvantartasHelp" value="<?php echo $adatok['nyilvantartas'];?>">
+            <input type="date" class="form-control" name="nyilvantartas" id="nyilvantartas" aria-describedby="nyilvantartasHelp">
         </div>
         <div class="mb-3 col-6">
             <label for="megjegyzes" class="form-label">Megjegyzés: </label>
-            <input type="text" class="form-control" name="megjegyzes" id="megjegyzes" aria-describedby="megjegyzesHelp" value="<?php echo $adatok['megjegyzes'];?>">
+            <input type="text" class="form-control" name="megjegyzes" id="megjegyzes" aria-describedby="megjegyzesHelp">
         </div>    
     </div>
     <div class="row">
